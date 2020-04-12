10cf98058e6b1280a660d33a323f0d68c173421b
Reyna292 <60242720+Reyna292@users.noreply.github.com>
2020-04-12 17:58:16 -0500

    Update validarlogin.php

--- quixlabZ/quixlab/themes/quixlab/validarlogin.php
+++ quixlabZ/quixlab/themes/quixlab/validarlogin.php
@@ -5,12 +5,13 @@
     
     if($reqlen > 0){
         require("connect_db.php");
-        $consulta = "SELECT * FROM usuarios WHERE usuario='$usr' and contraseña='$pass'";
+        $consulta = "SELECT * FROM usuarios WHERE usuario='".$usr."' and contraseña='".$pass."'";
         echo $consulta;
         $resultado=mysqli_query($link, $consulta);
         $filas=mysqli_num_rows($resultado);
         $x = mysqli_fetch_array($resultado);
         if ($filas > 0 ){
+            echo "entrooo";
             session_start();
             $_SESSION['usuario'] = $usr;
             echo ("<script language='javascript'> 
