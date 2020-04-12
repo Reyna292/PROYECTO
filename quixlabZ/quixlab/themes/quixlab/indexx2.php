3f022d4593b3660dbf0bd51effc0f4f8be9188b1
Reyna292 <60242720+Reyna292@users.noreply.github.com>
2020-04-11 21:09:51 -0500

    conn

--- quixlabZ/quixlab/themes/quixlab/indexx2.php
+++ quixlabZ/quixlab/themes/quixlab/indexx2.php
@@ -1,6 +1,6 @@
 <?php  
 	
-	 $link = new mysqli("localhost","root","","revolution");
+	 $link = new mysqli("localhost","admin","93ab9f73989e766a77c306ba3e6f7cb8d95309f36378ceed","revolution");
 	 $salida = "";
 	 $sql = "select id,plataforma,serial,cantidad from consolas;";
 
